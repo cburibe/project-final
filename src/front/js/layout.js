367fc7a045b811c83a18247a9005331aeea8ddc0
Robinsoncifuentes <robinsonmunoz4@gmail.com>
2022-03-16 18:04:01 +0000

    en dessa

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,41 +1,32 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import FormRegistro from "./pages/formulario/registro";
-import { Mainpage } from "./pages/mainpage/mainpage";
-
 import { Profile } from "./pages/profileclient/profile";
 
 import { Feed } from "./pages/feedclient/feed.js";
 import { Publication_uploader } from "./pages/create_publication/publication_uploader.js";
+import { Home } from "./pages/home";
+import { Demo } from "./pages/demo";
+import { Single } from "./pages/single";
 import { Config } from "./pages/config";
 import { RecommendedHotels } from "./pages/recommendedHotels";
+import injectContext from "./store/appContext";
 
 import { Menu } from "./component/navbar";
 import { Footer } from "./component/footer";
-import MyMap from "./pages/Map/view.map";
 
 const Layout = () => {
-  //the basename is used when your project is published in a subdirectory and not in the root of the domain
-  // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
   const basename = process.env.BASENAME || "";
 
   return (
-    <>
+    <div>
       <BrowserRouter basename={basename}>
-        <Menu />
         <Switch>
           <Route exact path="/">
-            <Mainpage />
+            <Home />
           </Route>
-          <Route exact path="/registro">
-            <FormRegistro />
-          </Route>
-          <Route exact path="/map">
-            <MyMap />
-          </Route>
-          <Route exact path="/feed">
-            <Feed />
+          <Route exact path="/demo">
+            <Demo />
           </Route>
           <Route exact path="/config">
             <Config />
@@ -43,20 +34,25 @@ const Layout = () => {
           <Route exact path="/recommendedhotels">
             <RecommendedHotels />
           </Route>
-
+          <Route exact path="/single/:theid">
+            <Single />
+          </Route>
           <Route exact path="/profile">
             <Profile />
           </Route>
-          <Route exact path="/publication_uploader">
-            <Publication_uploader />
-          </Route>
           <Route>
-            <h1>Not found!</h1>
+            <Route exact path="/feed">
+              <Feed />
+            </Route>
+            <Route exact path="/publication_uploader">
+              <Publication_uploader />
+            </Route>
           </Route>
+          Icon
+          <h1>Not found!</h1>
         </Switch>
-        <Footer />
       </BrowserRouter>
-    </>
+    </div>
   );
 };
 export default injectContext(Layout);
