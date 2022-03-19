13d16b81c45d2e2cf6be2aa26c33bffd564fb8fd
InTr0p <christianolivare01@gmail.com>
2022-03-19 05:02:05 +0000

    cambios front y feed christian

--- src/api/routes.py
+++ src/api/routes.py
@@ -7,13 +7,6 @@ from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
 
-
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
-
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
 @api.route('/peoples', methods=['GET'])
 def all_people():
     peoples = People.query.all()
--- src/front/js/component/profileclient/backgroundimgprofile.js
+++ src/front/js/component/profileclient/backgroundimgprofile.js
@@ -1,58 +1,39 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { Imgprofile } from "../profileclient/frontimgprofile";
 import { GiHouse } from "react-icons/gi";
-import { FaSearchLocation } from "react-icons/fa";
 import { FiSettings } from "react-icons/fi";
-import { ImBin } from "react-icons/im";
-import { MdOutlineAddCircle } from "react-icons/md";
 import "../../../styles/profileclient/backgroundimgprofile.css";
 import { Link } from "react-router-dom";
-import { IconContext } from "react-icons";
 
 export const Imgbackground = (props) => {
-  // const Propsimgbackground = {
-  //   linkImageback: "https://picsum.photos/200/300?grayscale",
-  //   imagepost: "https://picsum.photos/200/300",
-  //   user1: "carlos",
-  //   caption: "Mi primera foto",
-  // };
+  const Propsimgbackground = {
+    linkImageback: "https://picsum.photos/1600/1000?grayscale",
+    imagepost: "https://picsum.photos/1600/1000",
+    user1: "carlos",
+    caption: "Mi primera foto",
+    imgbackground: "https://picsum.photos/1600/1000",
+  };
   return (
-    <>
-      <IconContext.Provider value={{ color: "black", size: 50 }}>
-        <div
-          className=" backgroundimg "
-          style={{
-            background: "url(https://picsum.photos/200/300?grayscale)",
-            height: "25rem",
-          }}
+    <div className="divfondo position-relative">
+      <img src={Propsimgbackground.imgbackground} className="fondo " />
+      <Link to="/config">
+        <button
+          type="button"
+          class=" Imgbackground_buttons_info d-flex justify-content-end position-relative position-absolute top-0 end-0 mt-2 me-4 btn-info rounded "
         >
-          <Link className="nav-link active " to="/feed">
-            <GiHouse className="houseicon_Imgbackground ms-0" />
-          </Link>
-          <Link className="nav-link active " to="/map">
-            <FaSearchLocation className=" location2icon_Imgbackground " />
-          </Link>
+          <FiSettings className="settingsicon_Imgbackground" />
+        </button>
+      </Link>
 
-          <Link to="/config">
-            <FiSettings className=" settingsicon_Imgbackground d-flex justify-content-end  " />
-          </Link>
-          <Imgprofile linkImageFrony="https://picsum.photos/200" />
+      <Link className="nav-link active " to="/feed">
+        <button className="btn-info ms-0 position-relative position-absolute top-0 start-0 ms-4 mt-1 rounded">
+          <GiHouse className="houseicon_Imgbackground " />
+        </button>
+      </Link>
 
-          <span type="button" className=" Imgbackground_binicon ms-4 ">
-            <ImBin />
-          </span>
-          <Link to="/publication-uploader">
-            <MdOutlineAddCircle className="Imgbackground_addicon  ms-4" />
-          </Link>
-        </div>
-      </IconContext.Provider>
-    </>
+      <Imgprofile linkImageFrony="https://picsum.photos/200" />
+    </div>
   );
-};
-
-Imgbackground.propTypes = {
-  linkImage: PropTypes.any,
 };
 
 export default Imgbackground;
