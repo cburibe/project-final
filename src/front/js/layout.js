39b91acc48c5aa04dea34a696bb800d65315e355
Robinsoncifuentes <robinsonmunoz4@gmail.com>
2022-03-31 21:30:30 +0000

    en dev

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,17 +1,10 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { Profile } from "./pages/profileclient/profile";
-import { Feed } from "./pages/feedclient/feed.js";
-import { Publication_uploader } from "./pages/create_publication/publication_uploader.js";
 import { Config } from "./pages/config";
-import { RecommendedHotels } from "./pages/recommendedHotels";
-import MyMap from "./pages/Map/view.map";
 import injectContext from "./store/appContext";
 import Mainpage from "./pages/mainpage/mainpage";
-import Goback from "./pages/Map/goback";
-import { Demo } from "./pages/profileclient/demo";
-import { Prueba } from "./pages/prueba";
 import NewLogin from "./pages/mainpage/newlogin";
+import Perfil from "./pages/perfil/perfiluser";
 
 const Layout = () => {
   const basename = process.env.BASENAME || "";
@@ -23,34 +16,14 @@ const Layout = () => {
           <Route exact path="/">
             <Mainpage />
           </Route>
-          <Route exact path="/demo">
-            <Demo />
-          </Route>
           <Route exact path="/login">
             <NewLogin />
           </Route>
           <Route exact path="/config">
             <Config />
           </Route>
-          <Route exact path="/map">
-            <Goback />
-            <MyMap />
-          </Route>
-          <Route exact path="/recommendedhotels">
-            <RecommendedHotels />
-          </Route>
-          <Route exact path="/profile">
-            <Profile />
-          </Route>
-
-          <Route exact path="/feed">
-            <Feed />
-          </Route>
-          <Route exact path="/publication_uploader">
-            <Publication_uploader />
-          </Route>
-          <Route>
-            <Prueba />
+          <Route exact path="/perfil">
+            <Perfil />
           </Route>
           <Route>
             <h1>Not found!</h1>
---
+++ src/front/js/pages/perfil/perfiluser.js
@@ -0,0 +1,138 @@
+import React from "react";
+import { FiMapPin, FiUser, FiImage } from "react-icons/fi";
+import { MdHome, MdOutlineSettingsApplications } from "react-icons/md";
+import "/workspace/project-final/src/front/styles/perfiluser.css";
+
+const Perfil = () => {
+  return (
+    <>
+      <div className="row">
+        <div className="col-md-12 mx-auto">
+          <MdHome className="iconhouse justify-content-between" />
+          <MdOutlineSettingsApplications className="iconconfig " />
+          <div className="todo shadow rounded overflow-hidden">
+            <div className="px-4 pt-0 pb-5 cover">
+              <div className="media align-items-end profile-head">
+                <div className="profile mr-3">
+                  <img
+                    src="https://www.entornoturistico.com/wp-content/uploads/2016/11/trazar-ruta.jpg"
+                    alt="..."
+                    width={290}
+                    className="profi mb-2 img-thumbnail"
+                  />
+                </div>
+                <div className="media-body mb-5 text-white">
+                  <button
+                    type="button"
+                    className="boton btn btn-outline-success btn-sm"
+                  >
+                    change photo
+                  </button>
+                  <h4 className="mt-0 mb-0">elsa capuntas</h4>
+                  <p className="small mb-4">
+                    {" "}
+                    <FiMapPin />
+                    Santiago, Chile
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="bg-light p-4 d-flex justify-content-end text-center">
+              <ul className="list-inline mb-0">
+                <li className="list-inline-item">
+                  <h5 className="font-weight-bold mb-0 d-block">215</h5>
+                  <small className="text-muted">
+                    {" "}
+                    <FiImage />
+                    Photos
+                  </small>
+                </li>
+                <li className="list-inline-item">
+                  <h5 className="font-weight-bold mb-0 d-block">745</h5>
+                  <small className="text-muted">
+                    {" "}
+                    <FiUser />
+                    Followers
+                  </small>
+                </li>
+                <li className="list-inline-item">
+                  <h5 className="font-weight-bold mb-0 d-block">340</h5>
+                  <small className="text-muted">
+                    {" "}
+                    <FiUser />
+                    Following
+                  </small>
+                </li>
+              </ul>
+            </div>
+            <div className="px-4 py-3">
+              <h5 className="mb-0">About Me</h5>
+              <div className="p-4 rounded shadow-sm bg-light">
+                <p className="font-italic mb-0">Web Developer</p>
+                <p className="font-italic mb-0">Lives in Santiago</p>
+                <p className="font-italic mb-0">Photographer</p>
+              </div>
+            </div>
+            <div className="py-4 px-4">
+              <div className="d-flex align-items-center justify-content-between mb-3">
+                <h5 className="mb-0">Recent photos</h5>
+              </div>
+              <div className="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
+                <div className="col-sm-12 col-md-6 col-lg-4 text-center">
+                  <div className="card card-cover h-100 overflow-hidden text-white rounded-5 shadow-lg">
+                    <img
+                      className="img-fluid"
+                      src="https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                    />
+                  </div>
+                </div>
+                <div className="col-sm-12 col-md-6 col-lg-4 text-center">
+                  <div className="card card-cover h-100 overflow-hidden text-white rounded-5 shadow-lg">
+                    <img
+                      className="img-fluid"
+                      src="https://images.pexels.com/photos/2559941/pexels-photo-2559941.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                    />
+                  </div>
+                </div>
+                <div className="col-sm-12 col-md-6 col-lg-4 text-center">
+                  <div className="card card-cover h-100 overflow-hidden text-white  rounded-5 shadow-lg">
+                    <img
+                      className="img-fluid"
+                      src="https://images.pexels.com/photos/2387418/pexels-photo-2387418.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                    />
+                  </div>
+                </div>
+                <div className="col-sm-12 col-md-6 col-lg-4 text-center">
+                  <div className="card card-cover h-100 overflow-hidden text-white rounded-5 shadow-lg">
+                    <img
+                      className="img-fluid"
+                      src="https://images.pexels.com/photos/1757363/pexels-photo-1757363.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                    />
+                  </div>
+                </div>
+                <div className="col-sm-12 col-md-6 col-lg-4 text-center">
+                  <div className="card card-cover h-100 overflow-hidden text-white rounded-5 shadow-lg">
+                    <img
+                      className="img-fluid"
+                      src="https://images.pexels.com/photos/4641265/pexels-photo-4641265.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                    />
+                  </div>
+                </div>
+                <div className="col-sm-12 col-md-6 col-lg-4 text-center">
+                  <div className="card card-cover h-100 overflow-hidden text-white  rounded-5 shadow-lg">
+                    <img
+                      className="img-fluid"
+                      src="https://images.pexels.com/photos/1591373/pexels-photo-1591373.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Perfil;
