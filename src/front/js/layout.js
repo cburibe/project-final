8bcd878102c4de7b98dfa571ec968ac2b0349920
InTr0p <christianolivare01@gmail.com>
2022-04-03 00:50:54 +0000

    dev/front

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -2,10 +2,8 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Config } from "./pages/config";
 import injectContext from "./store/appContext";
-import { Mainpage } from "./pages/mainpage/mainpage";
-import Goback from "./pages/Map/goback";
-import ScrollToTop from "./component/scrollToTop";
-import Mainpage from "./pages/mainpage/mainpage";
+import  Mainpage  from "./pages/mainpage/mainpage";
+
 import NewLogin from "./pages/mainpage/newlogin";
 import Perfil from "./pages/perfil/perfiluser";
 import Feed from "./pages/mainpage/newfeed/newfeed";
@@ -17,37 +15,8 @@ const Layout = () => {
 
   return (
     <div>
-      <BrowserRouter basename={basename}>
-        <ScrollToTop>
-          <Switch>
-            <Route exact path="/">
-              <Mainpage />
-            </Route>
-            <Route exact path="/config">
-              <Config />
-            </Route>
-            <Route exact path="/map">
-              <Goback />
-              <MyMap />
-            </Route>
-            <Route exact path="/recommendedhotels">
-              <RecommendedHotels />
-            </Route>
-            <Route exact path="/profile">
-              <Profile />
-            </Route>
+      <BrowserRouter>
 
-            <Route exact path="/feed">
-              <Feed />
-            </Route>
-            <Route exact path="/publication_uploader">
-              <Publication_uploader />
-            </Route>
-            <Route>
-              <h1>Not found!</h1>
-            </Route>
-          </Switch>
-        </ScrollToTop>
         <Switch>
           <Route exact path="/">
             <Mainpage />
@@ -64,7 +33,6 @@ const Layout = () => {
           <Route exact path="/profile/:username">
             <Profile />
           </Route>
-
           <Route exact path="/feed">
             <Feed />
           </Route>
@@ -72,6 +40,7 @@ const Layout = () => {
             <h1>Not found!</h1>
           </Route>
         </Switch>
+
       </BrowserRouter>
     </div>
   );
