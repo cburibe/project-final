478128c18e77f2d2879ae3d1f70b93b5c4d6d740
InTr0p <christianolivare01@gmail.com>
2022-04-04 03:08:55 +0000

    actualizacion dev

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -2,7 +2,7 @@ const getState = ({ getStore, getActions, setStore }) => {
   return {
     store: {
       base_url:
-        "https://3001-cburibe-projectfinal-hg3gwnsaklg.ws-us38.gitpod.io",
+        "https://3001-cburibe-projectfinal-50s9z39w0ii.ws-us38.gitpod.io",
       people: null,
       background: "white",
       initial: "white",
